bad4cd5a68354b9afa13a2aac6233a4b8537ce25
Bhae <142163449+windupstill@users.noreply.github.com>
2023-10-15 16:58:00 +0900

    Feat: 미니맵 맵생성 상호작용

--- Assets/02.Scripts/MiniMap.cs
+++ Assets/02.Scripts/MiniMap.cs
@@ -10,6 +10,8 @@ public class MiniMap : MonoBehaviour
         if (other.CompareTag("Grab"))
         {
             isMiniMapOn = true;
+            EnableAllMeshRenderers(transform);
+            Destroy(other.gameObject);
         }
     }
 
@@ -18,6 +20,22 @@ public class MiniMap : MonoBehaviour
         if (other.CompareTag("Grab"))
         {
             isMiniMapOn = false;
+        }
+    }
+
+    void EnableAllMeshRenderers(Transform obj)
+    {
+        // 현재 오브젝트의 Mesh Renderer를 찾아 활성화합니다.
+        MeshRenderer renderer = obj.GetComponent<MeshRenderer>();
+        if (renderer != null)
+        {
+            renderer.enabled = true;
+        }
+
+        // 하위 자식 오브젝트에 대해서 재귀적으로 호출합니다.
+        for (int i = 0; i < obj.childCount; i++)
+        {
+            EnableAllMeshRenderers(obj.GetChild(i));
         }
     }
 }
